a2fd1d5837b453948ab646a82a77df22442fce44
Daryl Tchienang <SESA636891@se.com>
2024-10-04 18:45:03 +0200

    refactor:
    Testing console deleted.

--- PostmanClone/obj/Debug/net7.0-windows/Views/MainWindow.g.i.cs
+++ PostmanClone/obj/Debug/net7.0-windows/Views/MainWindow.g.i.cs
@@ -53,7 +53,7 @@ namespace PostmanClone.Views {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/PostmanClone;component/views/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/PostmanClone;V1.0.0.0;component/views/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Views\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
